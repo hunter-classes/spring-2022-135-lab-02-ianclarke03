01fc5c9744135d7afa6af59a8d16644352ad2c99
ianclarke03 <83726782+ianclarke03@users.noreply.github.com>
2022-02-11 02:21:30 +0000

    G

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -2,24 +2,24 @@
 #include <iostream>
 using namespace std;
 
-// make an array
+
+int main()
+{
+  // make an array
 int fib[60];
 // first two terms are given
 fib[0] = 0;
 fib[1] = 1;
 // and all the following ones can be computed iteratively as
 //fib[i] = fib[i-1] + fib[i-2]
-
-
-int main()
-{
   for (int i = 2; i < 60; i++)
   {
     fib[i] = fib[i-1] + fib[i-2];
-    cout << fib[i];
-    i++;
+    cout << fib[i] << "" << endl;
     }
   return 0;
   }
 
 //Once your program is complete and works, check carefully the values printed on the screen. Specifically, what is happening when the numbers approach two billions? We expect that at some point the numbers start diverging from what they should be. Describe what you observe and explain why it is happening in a program comment.
+
+// because the 
